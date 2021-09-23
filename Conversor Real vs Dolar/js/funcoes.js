f28917cf0ff537f9469c64dc03e41959149e24ee
Wellington-Maciel <86801034+Wellington-Maciel@users.noreply.github.com>
2021-09-23 10:30:34 -0300

    Add files via upload

---
+++ Conversor Real vs Dolar/js/funcoes.js
@@ -0,0 +1,9 @@
+function calcularDolar(){
+
+    let real = document.getElementById("reais").value
+    let dolar = document.getElementById("dolar").value
+    
+    let resultado = real / dolar
+    
+    alert(resultado)
+    }
